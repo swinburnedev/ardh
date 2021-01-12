0ab6effca4e8b56d8e8850db546655617c255657
Andy Swinburne <andyswinburne@hotmail.com>
2021-01-12 20:57:13 +0000

    Contact img size

--- pages/contact.js
+++ pages/contact.js
@@ -14,12 +14,12 @@ const Contact = () => {
           <Header />
           {/* <Image src="/img/banner/contact.jpg" width={2000} height={843} /> */}
           <div>
-            <Image src="/img/banner/contact.jpg" width={750} height={523} layout="intrinsic" />
+            <Image src="/img/banner/contact.jpg" width={550} height={384} layout="intrinsic" />
             <style jsx>{`
                 background-color: #8a9ca6;
                 display: flex;
                 justify-content: center;
-                padding: 6%;
+                padding: 4%;
                 width: 100%;
               }
             `}</style>
