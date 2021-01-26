032dcaf6698f1804af66ab739da0cf5dd8eb1ff2
Andy Swinburne <andyswinburne@hotmail.com>
2021-01-26 20:18:42 +0000

    Randomise inspiration cards

--- pages/inspiration/index.js
+++ pages/inspiration/index.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Masonary from '../../components/Masonary';
+import { getImageCards, getQuoteCards } from '../../lib/util';
+import images from './images/images.json';
+import materials from './materials/materials.json';
+import quotes from './quotes/quotes.json';
 
 const Inspiration = ({cards}) => {
   return (
@@ -20,49 +24,15 @@ const Inspiration = ({cards}) => {
 };
 
 export async function getStaticProps() {
+  const imageCards = getImageCards(2, images);
+  const quoteCards = getQuoteCards(2, quotes);
+  const materialCards = getQuoteCards(2, materials);
   return {
     props: {
       cards: [
-        {
-          type: 'quote-top',
-          quote: 'Beauty perishes in life, but is immortal in art',
-          img: '/img/inspiration/basketball-court.jpg',
-          alt: '',
-          url: '/inspiration/quotes'
-        },
-        {
-          type: 'quote-bottom',
-          quote: 'If i had asked people what they wanted quote',
-          img: '/img/inspiration/basketball-court.jpg',
-          alt: '',
-          url: '/inspiration/materials'
-        },
-        {
-          type: 'image',
-          img: '/img/inspiration/decking-design.jpg',
-          alt: '',
-          url: '/inspiration/images'
-        },
-        {
-          type: 'quote-bottom',
-          quote: 'If i had asked people what they wanted quote',
-          img: '/img/inspiration/basketball-court.jpg',
-          alt: '',
-          url: '/inspiration/materials'
-        },
-        {
-          type: 'image',
-          img: '/img/inspiration/decking-design.jpg',
-          alt: '',
-          url: '/inspiration/images'
-        },
-        {
-          type: 'quote-top',
-          quote: 'Beauty perishes in life, but is immortal in art',
-          img: '/img/inspiration/basketball-court.jpg',
-          alt: '',
-          url: '/inspiration/quotes'
-        }
+        ...quoteCards,
+        ...imageCards,
+        ...materialCards
       ]
     },
   }
