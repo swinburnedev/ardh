a7bcfb15272184dfd20dee4ca72cb95e4dd6921f
Andy Swinburne <andyswinburne@hotmail.com>
2021-02-04 21:45:43 +0000

    Residential and square img card

--- components/cards/FullImage.js
+++ components/cards/FullImage.js
@@ -2,26 +2,27 @@ import Image from 'next/image';
 import styles from '../../styles/components/Cards.module.scss';
 import Wrappedink from './WrappedLink';
 
-const FullImage = ({img, alt, url}) => (
+const FullImage = ({img, thumb, alt, url}) => (
   <div className={styles.card}>
     <Wrappedink url={url}>
-      <div className={`${styles.image} ${styles.image__desktop}`}>
+      <div className={`${styles.image}`}>
         {/* TODO add layout="fill" / image variant */}
+        {/*  ${styles.image__desktop} */}
         <Image
-          height="765"
-          width="765"
-          src={img}
+          height="400"
+          width="400"
+          src={thumb ? thumb : 'https://via.placeholder.com/400'}
           alt={alt}
         />
       </div>
-      <div className={`${styles.image} ${styles.image__mobile}`}>
+      {/* <div className={`${styles.image} ${styles.image__mobile}`}>
         <Image
           src={img}
           height="468"
           width="472"
           alt={alt}
         />
-      </div>
+      </div> */}
     </Wrappedink>
   </div>
 )
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Masonary from '../../components/Masonary';
+import { getImageCards } from '../../lib/util';
+import civil from './civil/projects.json';
+import leisure from './leisure/projects.json';
+import residential from './residential/projects.json';
 
 const Projects = ({cards}) => {
   return (
@@ -20,50 +24,15 @@ const Projects = ({cards}) => {
 };
 
 export async function getStaticProps() {
+  const civilCards = getImageCards(2, civil, '1:1');
+  const leisureCards = getImageCards(2, leisure, '1:1');
+  const residentialCards = getImageCards(2, residential, '1:1');
   return {
     props: {
       cards: [
-        {
-          type: 'fify-image',
-          imgTop: '/img/inspiration/paint.png',
-          altTop: 'Alt top',
-          imgBottom: '/img/inspiration/basketball-court.jpg',
-          altBottom: 'Alt top',
-          url: '/projects/civil/gated-community'
-        },
-        {
-          type: 'quote-bottom',
-          quote: 'If i had asked people what they wanted quote',
-          img: '/img/inspiration/basketball-court.jpg',
-          alt: '',
-          url: '/test'
-        },
-        {
-          type: 'image',
-          img: '/img/inspiration/decking-design.jpg',
-          alt: '',
-          url: '/test'
-        },
-        {
-          type: 'quote-bottom',
-          quote: 'If i had asked people what they wanted quote',
-          img: '/img/inspiration/basketball-court.jpg',
-          alt: '',
-          url: '/test'
-        },
-        {
-          type: 'image',
-          img: '/img/inspiration/decking-design.jpg',
-          alt: '',
-          url: '/test'
-        },
-        {
-          type: 'quote-top',
-          quote: 'Beauty perishes in life, but is immortal in art',
-          img: '/img/inspiration/basketball-court.jpg',
-          alt: '',
-          url: '/test'
-        }
+        ...civilCards,
+        ...leisureCards,
+        ...residentialCards
       ]
     }
   }
