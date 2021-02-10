2158420af5f4c0409407df5cf27b4a1bddee418b
Andy Swinburne <andyswinburne@hotmail.com>
2021-02-10 17:02:12 +0000

    Mobile styling

--- components/PaginationRow.js
+++ components/PaginationRow.js
@@ -5,15 +5,19 @@ const PaginationRow = ({next, prev}) => {
   return (
     <div className={`container ${styles.pagination}`}>
       <div className={styles.pagination__prev}>
-        <p>Previous Project</p>
         <Link href={prev.url}>
-          <a>{prev.name}</a>
+          <a className={styles.pagination__subtitle}>Previous Project</a>
+        </Link>
+        <Link href={prev.url}>
+          <a className={styles.pagination__link}>{prev.name}</a>
         </Link>
       </div>
       <div className={styles.pagination__next}>
-        <p>Next Project</p>
         <Link href={next.url}>
-          <a>{next.name}</a>
+          <a className={styles.pagination__subtitle}>Next Project</a>
+        </Link>
+        <Link href={next.url}>
+          <a className={styles.pagination__link}>{next.name}</a>
         </Link>
       </div>
     </div>
